9a30f7f27a53aff1a1c90ad4ef8d621c47d873a1
takeryang <takeryang@gmail.com>
2019-07-04 23:44:06 +0800

    No5 file

--- products.py
+++ products.py
@@ -1,12 +1,24 @@
-# 讀取檔案
+import os
+
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('有這個檔案喔!')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+		print(products)
+else:
+	print('找不到檔案......')
+
+
+
+# 讀取檔案
+
+
 
 # 使用者輸入
 while True:
